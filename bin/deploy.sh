9cf6021f5653e2c20cc554675c7d391b941f44a3
Satyam World <58844449+satyam7world@users.noreply.github.com>
2024-06-30 10:42:55 +0530

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,14 @@
 
 #cd "$(dirname "$0")"
 
+cd /var/www/html
+
+# Check if cd was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to change directory to /var/www/html"
+  exit 1
+fi
+
 git stash
 git pull
 chmod 755 -R .
